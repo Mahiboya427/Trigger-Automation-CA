b09f370ff7927443c18d22ad2010f750ea95e033
Boya Mahesh <63939597+Mahiboya427@users.noreply.github.com>
2025-06-13 18:22:49 +0530

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,16 +1,36 @@
-window.onload = function () {
+define(['postmonger'], function (Postmonger) {
+  'use strict';
+
   const connection = new Postmonger.Session();
   let payload = {};
 
-  connection.trigger('ready');
+  // When window is ready
+  $(window).ready(function () {
+    connection.trigger('ready');
+    connection.trigger('requestTokens');
+    connection.trigger('requestEndpoints');
+  });
 
+  // When Journey loads the activity
   connection.on('initActivity', function (data) {
     if (data) {
       payload = data;
     }
+
+    // Mark activity as configured
+    payload['metaData'] = payload['metaData'] || {};
+    payload['metaData'].isConfigured = true;
+
+    connection.trigger('updateButton', {
+      button: 'next',
+      visible: true,
+      text: 'Done'
+    });
   });
 
-  document.getElementById('done').addEventListener('click', function () {
+  // When “Done” is clicked in UI
+  connection.on('clickedNext', function () {
+    // No need to set inArguments if you're just triggering an API
     connection.trigger('updateActivity', payload);
   });
-};
+});
