2534f48560349af60c8086b34d435a94a3eeb553
Boya Mahesh <boyamahesh568@gmail.com>
2025-06-19 12:43:49 +0530

    Add .gitignore and update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const axios = require('axios');
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -37,9 +37,9 @@ app.post('/activity/execute', async (req, res) => {
     }
 
     // 🔐 Auth
-    const clientId = '9pgjuv8byisnioxfbaqk820l';
-    const clientSecret = 'NCjoZUctnI8jYIcvWPsYtUup';
-    const accountId = '110007984';
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const accountId = process.env.ACCOUNT_ID;
     const authUrl = 'https://mc654h8rl6ypfygmq-qvwq3yrjrq.auth.marketingcloudapis.com/v2/token';
     const automationUrl = `https://mc654h8rl6ypfygmq-qvwq3yrjrq.rest.marketingcloudapis.com/automation/v1/automations/key:${automationKey}/actions/runallonce`;
 
